6bc85a717da45e57ad845423a6b6a9adb7bf0bc5
Alex Cittadini <Alex@Alexs-MacBook-Pro.local>
2017-08-18 17:13:12 +1000

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -8,19 +8,20 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req,res)=>{
-    var todo = new Todo({
-      text: req.body.text
-    });
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-    todo.save().then((doc)=>{
-      res.send(doc);
-    },(e)=>{
-      res.status(400).send(e);
-    });
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(e)=>{
+    res.status(400).send(e);
+  });
 });
 
 app.get('/todos',(req,res)=>{
@@ -34,22 +35,22 @@ app.get('/todos',(req,res)=>{
 app.get('/todos/:id',(req, res)=>{
   var id = req.params.id;
   if(!ObjectId.isValid(id)) {
-    res.status(404).send();
-  } else {
-    Todo.findById(id).then((todo)=>{
-      if (!todo) {
-        return res.status(404).send()
-      }
-      res.send(todo)
-    }).catch((e) => {
-      res.status(400).send()
-    });
-
+    return res.status(404).send();
   }
+  Todo.findById(id).then((todo)=>{
+    if (!todo) {
+      return res.status(404).send()
+    }
+    res.send(todo)
+  }).catch((e) => {
+    res.status(400).send()
+  });
+
+}
 });
 
-app.listen(3000, ()=>{
-  console.log('Started on port 3000');
+app.listen(port, ()=>{
+  console.log(`Started at port ${port}`);
 });
 
 module.exports = {app};
